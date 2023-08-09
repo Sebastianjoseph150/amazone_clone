7139bf0ffc2257b8ee862dbbbc2ea0aa5f872c15
Sebastianjoseph150 <sebastianjoseph150@gmail.com>
2023-08-09 11:06:39 +0530

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: must_be_immutable
+import 'package:amazone_clone/pages/subPages/all.dart';
+import 'package:amazone_clone/pages/subPages/movies.dart';
+import 'package:amazone_clone/pages/subPages/tv_shows.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  List allMoviesList = [];
+  List trendingMoviesList = [];
+  List tvShows = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.asset(
+          'assets/image/primelogo.png',
+          height: 25,
+        ),
+        actions: [
+          const Icon(
+            Icons.cast,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 20),
+          Icon(
+            Icons.account_circle_sharp,
+            color: Colors.blue.shade100,
+            size: 32,
+          ),
+          const SizedBox(width: 40)
+        ],
+        bottom: const TabBar(
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.transparent,
+          labelColor: Colors.white,
+          tabs: [
+            Tab(
+              text: 'All',
+            ),
+            Tab(
+              text: 'Movies',
+            ),
+            Tab(
+              text: 'TV shows',
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: TabBarView(
+        children: [
+          AllMovies(allMovies: allMoviesList),
+          Movies(trendingMovies: trendingMoviesList),
+          TvShows(tvShows: tvShows)
+        ],
+      ),
+    );
+  }
+}
